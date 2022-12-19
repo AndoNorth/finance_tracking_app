f8cc38de678cdd91df9c51e14f19ea0649fd0feb
Ando <55718737+AndoNorth@users.noreply.github.com>
2022-12-19 16:05:18 +0000

    Backend - API
    1) API using MVC architecture
    a) implemented API Endpoints to handle GET,POST,PUT and DELETE requests
    b) implemented TransactionDbContext for database interface (SqlServer) and added migration
    2) initial project setup files

---
+++ Backend/Backend/Controllers/TransactionController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Backend.Data;
+using Backend.Models;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        private readonly TransactionDbContext _context;
+        public TransactionController(TransactionDbContext context) => _context = context;
+        [HttpGet]
+        public async Task<IEnumerable<Transaction>> Get()
+        {
+            return await _context.Transactions.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(Transaction), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var transaction = await _context.Transactions.FindAsync(id);
+            return transaction == null ? NotFound() : Ok(transaction);
+        }
+        [HttpPost]
+        [ProducesResponseType(typeof(Transaction), StatusCodes.Status201Created)]
+        public async Task<IActionResult> Create([FromBody] Transaction transaction)
+        {
+            var dateTime = transaction.TransactionTime;
+            if ((dateTime == default(DateTime)) || (dateTime == DateTime.MinValue)) transaction.TransactionTime = DateTime.UtcNow; // dateTime.HasValue
+            await _context.Transactions.AddAsync(transaction);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetById), new {id = transaction.Id}, transaction);
+        }
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Update(int id, [FromBody] Transaction transaction)
+        {
+            if(transaction.Id != id) return BadRequest();
+
+            _context.Entry(transaction).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> DeleteById(int id)
+        {
+            var transactionToDelete = await _context.Transactions.FindAsync(id);
+            if (transactionToDelete == null) return NotFound();
+
+            _context.Transactions.Remove(transactionToDelete);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ Backend/Backend/Models/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Backend.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Summary { get; set; }
+        public string ? Description { get; set; }
+        [Required]
+        public double Amount { get; set; }
+        [Required]
+        public TransactionTypes TransactionType { get; set; }
+        public enum TransactionTypes
+        {
+            Ingoing, Outgoing
+        }
+        public DateTime TransactionTime { get; set; }
+    }
+}
