3476a7980218eeb1918df59e84ef60d38c6d9fd0
Ando <55718737+AndoNorth@users.noreply.github.com>
2022-12-21 14:24:44 +0000

    Backend - cleanup
    1) move cors to development only environment

--- BackendAPI/BackendAPI/Program.cs
+++ BackendAPI/BackendAPI/Program.cs
@@ -12,10 +12,6 @@ builder.Services.AddDbContext<TransactionDbContext>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
-{
-    builder.WithOrigins("http://localhost:5173").AllowAnyMethod().AllowAnyHeader();
-}));
 
 var app = builder.Build();
 
@@ -24,9 +20,15 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+    {
+        // builder.WithOrigins("http://localhost:5173").AllowAnyMethod().AllowAnyHeader();
+        builder.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin();
+    }));
+    app.UseCors("corsapp");
 }
 
-app.UseCors("corsapp");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
